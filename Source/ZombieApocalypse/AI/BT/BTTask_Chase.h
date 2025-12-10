3d4dddbd5636348a2f6013b1070025fa6355be4a
Panteleimon Arizanis <44447795+pantelisariz@users.noreply.github.com>
2025-12-10 19:54:41 +0100

    merge

---
+++ Source/ZombieApocalypse/AI/BT/BTTask_Chase.h
@@ -0,0 +1,26 @@
+// Copyright University of Inland Norway
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_Chase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZOMBIEAPOCALYPSE_API UBTTask_Chase : public UBTTaskNode
+{
+	GENERATED_BODY()
+public:	
+	explicit UBTTask_Chase(FObjectInitializer const& ObjectInitializer);
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blackboard", meta = (AllowPrivateAccess = "true"))
+	FBlackboardKeySelector In_TargetLocation;
+private:
+	UPROPERTY()
+	FTimerHandle DelayHandle;
+};
